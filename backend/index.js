6f504c67adddfb41e99ec0d643fd3e6953c2144f
Ankit Kumar Singh <ankitkumarsingh221b072@gmail.com>
2025-02-23 22:56:03 +0530

    Added routing

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,17 @@
-console.log("ready ?");
+const express = require("express");
+const cors = require("cors");
+const app = express()
+const userRouter = require("./routes/user")
+const adminRouter = require("./routes/admin")
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/v1/user",userRouter);
+app.use("/api/v1/admin",adminRouter);
+
+
+
+
+app.listen(3000,()=>{
+    console.log("BE running on port 3000")
+})
